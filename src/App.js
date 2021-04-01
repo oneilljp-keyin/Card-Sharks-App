72cbdee156181eed665ab80660a1e3c781579ffe
oneilljp-keyin <john.oneill@keyin.com>
2021-03-31 21:42:15 -0230

    finished basic functioning of drawing cards

--- src/App.js
+++ src/App.js
@@ -1,119 +1,139 @@
 import { useEffect, useState } from "react";
 import './App.css';
-// import NewDeck from "./NewDeck";
 import { v4 as uuidv4 } from 'uuid'; // then use uuidv4() to insert id
+import csLogo from "./assets/Card_Sharks_2019_logo.png";
 
 function App() {
+  const [directions, setDirections]             = useState("Welcome To Card Sharks - Press Start to Play");
   const [roundNum, setRoundNum]                 = useState(0);
+
   const [currentKey, setCurrentKey]             = useState();
   const [currentCardValue, setCurrentCardValue] = useState()
-  const [deckID, setDeckID]                     = useState();
   const [nextCards, setNextCards]               = useState();
-  const [roundResult, setRoundResult]           = useState("Good Luck");
+  const [drawCards, setDrawCards]               = useState();
+  const [roundResult, setRoundResult]           = useState("Good Luck!");
   const [turnsRemain, setTurnsRemain]           = useState(3);
-  const [swapOut, setSwapOut]                   = useState(true);
   const [displayCards, setDisplayCards]         = useState([]);
+  const [startButton, setStartButton]           = useState("Start");
+  const [startDisplay, setStartDisplay]         = useState(false);
+
+  const [showBtns, setShowBtns]                 = useState(false);
+  const [highBtn, setHighBtn]                   = useState(false);
+  const [lowBtn, setLowBtn]                     = useState(false);
+  const [swapOut, setSwapOut]                   = useState(false);
   
   useEffect(() => {
-    console.log("This Should Be Step #1");
     const fetchDeck = async () => {
-    
-      let data = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
-      let json = await data.json();
+      let deck = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+      let deckJSON = await deck.json();
+      console.log(deckJSON);
 
-      setDeckID(json.deck_id);
+      let first = await fetch(`https://deckofcardsapi.com/api/deck/${deckJSON.deck_id}/draw/?count=7`);
+      let firstJSON = await first.json();
+  
+      setNextCards(firstJSON);
 
-      console.log(json);
-      console.log(deckID);
+      let second = await fetch(`https://deckofcardsapi.com/api/deck/${deckJSON.deck_id}/draw/?count=4`)
+      let secondJSON = await second.json();
+  
+      setDrawCards(secondJSON);
+  
+      console.log(firstJSON);
+      console.log(firstJSON);
     }
 
     fetchDeck();
   }, []);
 
-  const fetchNewDeck = async () => {
-    console.log("This Should Be Step #2");
-
-    let deckNum = deckID;
-    console.log(deckNum)
-  
-    let data = await fetch(`https://deckofcardsapi.com/api/deck/${deckNum}/draw/?count=7`);
-    let json = await data.json();
-
-    setNextCards(json);
-
-    console.log(json);
-
-    return
-  }
-
-  function nextCardValue() {
-    let data = nextCards;
-    if (data.cards[roundNum].value === "ACE")   {setCurrentCardValue(14);} else
-    if (data.cards[roundNum].value === "KING")  {setCurrentCardValue(13);} else 
-    if (data.cards[roundNum].value === "QUEEN") {setCurrentCardValue(12);} else 
-    if (data.cards[roundNum].value === "JACK")  {setCurrentCardValue(11);} else
-       {setCurrentCardValue(data.cards[roundNum].value)};
-
-    return
+  function cardValue(cardValue) {
+    let value = cardValue;
+    if (value === "ACE")   {return 14;} else
+    if (value === "KING")  {return 13;} else 
+    if (value === "QUEEN") {return 12;} else 
+    if (value === "JACK")  {return 11;} else
+       {return value};
   }
 
   function showFirstCard() {
-    fetchNewDeck();
     let data = nextCards;
-    console.log(data);
+
+    setShowBtns(true);
+    setStartDisplay(true);
+    setStartButton("Good Luck!");
+
+    let nextKey = uuidv4();
+    setCurrentKey(nextKey);
+
+    let firstValue = parseInt(cardValue(data.cards[roundNum].value));
+    setCurrentCardValue(firstValue);
 
     let outputArray = [];
 
-    let nextKey = uuidv4();
-    setCurrentKey(nextKey);
-    
-    outputArray.push(<div className="card" key={nextKey}><img src={data.cards[roundNum].image} alt={data.cards[roundNum].code} className="Display-Card" /></div>);
+    outputArray.push(
+      <div className="card" key={nextKey}>
+        <img src={data.cards[roundNum].image} alt={data.cards[roundNum].code} className="Display-Card" />
+      </div>
+    );
+
     setDisplayCards(outputArray);
-
-    console.log(currentKey);
-    console.log(currentCardValue);
+    setRoundNum(previousValue => previousValue + 1);
   }
 
-  function nextCardHigher() {
-    let currentValue = currentCardValue;
+  function nextCard(option) {
     let cards = nextCards;
+    let num = roundNum;
+    let hiLow = option;
 
-    setRoundNum(prevState => [...prevState, prevState + 1]);
-    let nextCard = cards.cards[roundNum].value;
+    let NextCardValue = parseInt(cardValue(cards.cards[num].value));
 
-    if (currentValue < nextCard) {
-      setRoundResult("Correct");
+    if (NextCardValue > currentCardValue) {
+      if (hiLow === "higher") {
+        setRoundResult("Correct");
+      } else {
+        setRoundResult("Incorrect");
+        setTurnsRemain(previousValue => previousValue - 1);
+      }
     } else {
-      setRoundResult("Incorrect");
-      setTurnsRemain(turnsRemain - 1);
+      if (hiLow === "lower") {
+        setRoundResult("Correct");
+      } else {
+        setRoundResult("Incorrect");
+        setTurnsRemain(previousValue => previousValue - 1);
+      }
     }
-    let nextKey = uuidv4();
-    setCurrentKey(nextKey);
-    let showNextCard = `<div className="card" key=${nextKey}><img src=${cards.cards[roundNum].image} alt=${cards.cards[roundNum].code} className="Display-Card" /></div>`;
+    let NextKey = uuidv4();
+    setCurrentKey(NextKey);
+    let AddToArray = [];
+    AddToArray.push(
+      <div className="card" key={NextKey}>
+        <img src={cards.cards[num].image} alt={cards.cards[num].code} className="Display-Card" />
+      </div>
+    );
 
-    setDisplayCards(prevArray => [...prevArray, showNextCard]);
-
-    nextCardValue();
-  }
-
-  function nextCardLower() {
-    return
+    setCurrentCardValue(NextCardValue);
+    setDisplayCards(oldArray => [...oldArray, AddToArray]);
+    setRoundNum(previousValue => previousValue + 1);
+    console.log(displayCards);
   }
 
   function swapOutCard() {
+    setSwapOut(true);
     return
   }
 
   return (
     <div className="App">
-      <button onClick={showFirstCard}>Start the Game</button><br />
+      <img className="logo" src={csLogo} alt="Card Sharks" />
+      <div className="header"><button disabled={startDisplay} onClick={showFirstCard}>{startButton}</button></div>
       <div className="Card-Container">
         {displayCards}<br />
       </div>
-      <div>{roundResult}</div>
-      <button onClick={nextCardLower}>Lower</button>
-      <button onClick={nextCardHigher}>Higher</button>
-      <button onClick={swapOutCard}>Swap Out Card</button>
+      <div className="directions">{directions}</div>
+      <div className="buttons" style={{ display: showBtns ? "block" : "none" }}>{roundResult}<br />
+      <button id="higherBtn" disabled={highBtn} onClick={() => nextCard("higher")}>Higher</button><br /><br />
+      <button id="lowerBtn"  disabled={lowBtn} onClick={() => nextCard("lower")}>Lower</button><br />
+      <button id="swapBtn"   disabled={swapOut} onClick={swapOutCard}>Swap Out Card</button><br />
+      Turns Left: {turnsRemain}</div>
     </div>
   );
 }
